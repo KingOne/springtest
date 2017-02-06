7c826cf6372e37f462078f311b2600e819ca8890
KingOne <178007127@qq.com>
2017-02-06 13:39:37 +0800

    ioc

---
+++ src/main/java/com/easy/a_ioc/UserService.java
@@ -0,0 +1,5 @@
+package com.easy.a_ioc;
+
+public interface UserService {
+	public void addUser();
+}
---
+++ src/main/java/com/easy/a_ioc/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.easy.a_ioc;
+
+
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public void addUser() {
+		System.out.println("com.easy.a_ioc");
+	}
+
+}
